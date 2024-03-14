f3c17cc79fe889a6f41353423c713f6b3b5021e3
saurabh <saurabh2113076@akgec.ac.in>
2024-03-14 23:38:38 +0530

    some code changes

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ require('dotenv').config();
 require("./db/conn");
 const router = require('./routes/router');
 const cors = require("cors");
-const port = 3000;
+const PORT = process.env.PORT || 3001
 
 // app.get("/",(req,res)=>{
 //   res.status(201).json("server created")
 // });
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(cors());
 
-app.listen(process.env.PORT,()=>{
-  console.log(`server start at port no :${port}`)
+
+app.listen(PORT,()=>{
+  console.log(`server start at port no :${PORT}`)
 })
 
